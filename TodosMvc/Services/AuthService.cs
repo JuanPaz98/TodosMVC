5d935667f95f1313ecca8d3a13413759867a5a3b
Juan Paz <jjfer032@gmail.com>
2025-03-23 13:09:28 -0500

    Refactor GenerateJwtToken method

--- TodosMvc/Services/AuthService.cs
+++ TodosMvc/Services/AuthService.cs
@@ -21,26 +21,44 @@ namespace TodosMvc.Services
         public string GenerateJwtToken(LoginVm model)
         {
             var jwtConfig = _configuration.GetSection("Jwt");
-            var key = Encoding.UTF8.GetBytes(jwtConfig["Key"]);
-            var creds = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+            var keyString = jwtConfig["Key"];
+            var issuer = jwtConfig["Issuer"];
+            var audience = jwtConfig["Audience"];
 
-            var claims = new[]
+            if (string.IsNullOrEmpty(keyString) || string.IsNullOrEmpty(issuer) || string.IsNullOrEmpty(audience))
+            {
+                return "";
+            }
+
+            var keyBytes = Encoding.UTF8.GetBytes(keyString);
+
+            if (keyBytes.Length < 32) 
+            {
+                throw new ArgumentException("JWT signing key must be at least 32 bytes long.");
+            }
+
+            var creds = new SigningCredentials(new SymmetricSecurityKey(keyBytes), SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
             {
                 new Claim(JwtRegisteredClaimNames.Sub, model.Username),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(JwtRegisteredClaimNames.Aud, _configuration["Jwt:Audience"]),
-                new Claim(JwtRegisteredClaimNames.Iss, _configuration["Jwt:Issuer"])
+                new Claim(ClaimTypes.Name, model.Username)
             };
 
-            var token = new JwtSecurityToken(
-               issuer: _configuration["Issuer"],
-               audience: _configuration["Audience"],
-               claims: claims,
-               expires: DateTime.UtcNow.AddHours(1),
-               signingCredentials: creds
-            );
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(1),
+                Issuer = issuer,
+                Audience = audience,
+                SigningCredentials = creds
+            };
 
-            return new JwtSecurityTokenHandler().WriteToken(token);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
 
         public void StoreJwtInCookie(string token)
